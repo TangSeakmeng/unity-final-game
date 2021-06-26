f9de81976efbc69a23d44d7e3e4b6215343517e4
Lim Hangkuong <hangkuong@Lims-MacBook-Pro.local>
2021-06-26 22:37:52 +0700

    added portal and new enviromenet

---
+++ Assets/Scripts/CollisionEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionEvent : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name + " collided with " + gameObject.name);
+        collision.transform.position = new Vector3(x: -100, y: 1, z: -400);
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name + " colliding with " + gameObject.name);
+        collision.transform.position = new Vector3(x: -100, y: 1, z: -400);
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name + " ended collision with " + gameObject.name);
+        collision.transform.position = new Vector3(x: 12, y: 1, z: 15);
+
+
+    }
+}
---
+++ Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+    GameObject winEffects;
+    public GameObject Charater;
+    Vector3 translate;
+
+    private void Start()
+    {
+        winEffects = transform.GetChild(0).gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Charater)
+        {
+            //winEffects.SetActive(true);
+            //ballGO.transform
+            Charater.transform.position = new Vector3(12, 0, 12);
+            Debug.Log("Collided");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == Charater)
+        {
+            //winEffects.SetActive(true);
+            Charater.transform.position = new Vector3(12, 0, 12);
+            Debug.Log("Collided");
+        }
+    }
+}
